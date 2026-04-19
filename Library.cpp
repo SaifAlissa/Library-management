200699ad9e3a18e5c3ed6adc08841ddd9fbaf10a
SaifAlissa <saifalissa2005@gmail.com>
2026-04-19 17:50:18 +0300

    Main

--- Library.cpp
+++ Library.cpp
@@ -1,1 +1,34 @@
+ #include <iostream> 
+ using namespace std;
+
+ class Library
+ {
+ private:
   
+ public:
+    Library();
+
+    bool addAdmin(string a, string b);
+    bool addLibrarian(string a, string b);
+    bool addMember(string a, string b);
+    bool addBook(string a, string b, int c);
+    int loginAdmin(string a, string b);
+    int loginLibrarian(string a, string b);
+    int loginMember(string a, string b);
+    void displayAdminInfo(int a);
+    bool addAdmin(string a, string b);
+    void displayStatistics();
+    void displayLibrarianInfo(int a);
+    bool addBook(string a, string b, int c);
+    void displayAllBooks();
+    void searchBooks(string a);
+    void displayMembers();
+    void displayBorrowRecords();
+    int getMemberIdByIndex(int a);
+    void displayMemberInfo(int a);
+    void displayAvailableBooks();
+    bool borrowBook(int a, int b);
+    bool returnBook(int a, int b);
+    void displayBorrowedBooksForMember(int a);
+    
+ };
